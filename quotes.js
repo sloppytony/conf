d015d26c992136ba64c8c23519d729f45f34faa0
anthony <anthonytostado@gmail.com>
2021-01-17 13:40:30 -0800

    fixed responsive features including conf img position and animation

---
+++ quotes.js
@@ -0,0 +1,5 @@
+let quotes = ['The man who says he can, and the man who says he can not… Are both correct',
+'Your life is what your thoughts make it',
+'Real knowledge is to know the extent of one’s ignorance',
+'The man who asks a question is a fool for a minute, the man who does not ask is a fool for life'
+];
