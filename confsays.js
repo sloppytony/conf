069acf41f9439c6092d860861ac215676dc81cda
anthony <anthonytostado@gmail.com>
2021-01-17 14:21:14 -0800

    added refresh button and quotes to quotes array

--- confsays.js
+++ confsays.js
@@ -3,7 +3,7 @@ const btn = document.getElementById("btn");
 
 btn.addEventListener('click', ()=>{
     const container = document.getElementById("container");
-    const randomArray = quotes[Math.floor(Math.random() * 4)];
+    const randomArray = quotes[Math.floor(Math.random() * 22)];
     const p = document.createElement('p');
     p.textContent= randomArray;
     
@@ -12,8 +12,8 @@ btn.addEventListener('click', ()=>{
    img.classList.add('active');
 });
 
-// let quotes = ['The man who says he can, and the man who says he can not… Are both correct',
-// 'Your life is what your thoughts make it',
-// 'Real knowledge is to know the extent of one’s ignorance',
-// 'The man who asks a question is a fool for a minute, the man who does not ask is a fool for life'
-// ];
+const i = document.getElementById('icon');
+
+i.addEventListener('click', ()=>{
+    location.reload(false);
+})
